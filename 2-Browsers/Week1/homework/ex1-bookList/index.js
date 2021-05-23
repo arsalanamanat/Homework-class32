f7e5ae36b411e432485a55d20533ff7c16edc5c5
Engr Arslan Amanat <78694077+arsalanamanat@users.noreply.github.com>
2021-05-23 17:50:03 +0200

    Completed Exercise No1 . run npm test passed for all

--- 2-Browsers/Week1/homework/ex1-bookList/index.js
+++ 2-Browsers/Week1/homework/ex1-bookList/index.js
@@ -2,19 +2,15 @@
 /*
   
  ** Exercise 1: The book list **
-
   I'd like to display my three favorite books inside a nice webpage!
-
   1. Iterate through the array of books.
   2. For each book, create a `<p>`
   element with the book title and author.
   3. Use a `<ul>`  and `<li>` to display the books.
   4. Add an `<img>` to each book that links to a URL of the book cover.
   5. Change the style of the book depending on whether you have read it(green) or not(red).
-
   The end result should look something like this:
   https: //hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
-
 */
 //cspell: enable
 
@@ -41,6 +37,42 @@ const myBooks = [
 
 function createBookList(books) {
   // your code goes in here, return the ul element
+  const newUl = document.createElement('ul');
+  let list;
+  let image;
+
+  // Creating Elements by Using the For Loop
+  for (let i = 0; i < books.length; i++) {
+    newUl
+      .appendChild((list = document.createElement('li')))
+      .appendChild(
+        (para = document.createElement('p'))
+      ).textContent = `${books[i].title} - ${books[i].author} `;
+
+    list.appendChild((image = document.createElement('IMG')));
+    list.style.margin = '20px 20px';
+    list.style.padding = '10px';
+    image.style.maxWidth = '60%';
+    image.alt = `Authors : ${books[i].author}`;
+
+    if (books[i].author === 'Don Norman') {
+      image.src = './assets/the_design_of_everyday_things.jpg';
+    } else if (books[i].author === 'Brian Christian') {
+      image.src = './assets/the_most_human_human.jpg';
+    } else {
+      image.src = './assets/the_pragmatic_programmer.jpg';
+    }
+
+    if (books[i].alreadyRead === true) {
+      list.style.backgroundColor = 'green';
+    } else {
+      list.style.backgroundColor = 'red';
+    }
+  }
+  newUl.style.listStyle = 'none';
+  newUl.style.display = 'inline-flex';
+
+  return newUl;
 }
 
 const ulElement = createBookList(myBooks);
