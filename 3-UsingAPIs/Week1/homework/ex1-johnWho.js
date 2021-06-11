dc66fd6f7b444ebfd2b370fbe8dcd9cf900fe18f
Engr Arslan Amanat <78694077+arsalanamanat@users.noreply.github.com>
2021-06-11 19:45:27 +0200

    Ex1 Solved All Units Passed NPM Test

--- 3-UsingAPIs/Week1/homework/ex1-johnWho.js
+++ 3-UsingAPIs/Week1/homework/ex1-johnWho.js
@@ -7,20 +7,28 @@ Rewrite this function, but replace the callback syntax with the Promise syntax:
   didn't pass in a first name!"
 ------------------------------------------------------------------------------*/
 // TODO see above
-const getAnonName = (firstName, callback) => {
-  setTimeout(() => {
-    if (!firstName) {
-      callback(new Error("You didn't pass in a first name!"));
-      return;
-    }
-
-    const fullName = `${firstName} Doe`;
-
-    callback(fullName);
-  }, 1000);
+const getAnonName = (firstName) => {
+  const fullName = `${firstName} Doe`;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (firstName) {
+        resolve(fullName);
+      } else {
+        reject(new Error("You didn't pass in a first name!"));
+      }
+    }, 1000);
+  });
 };
 
-getAnonName('John', console.log);
+getAnonName('John')
+  .then((data) => {
+    console.log(data);
+    return getAnonName();
+  })
+
+  .catch((error) => {
+    console.log(error);
+  });
 
 // ! Do not change or remove the code below
 module.exports = getAnonName;
